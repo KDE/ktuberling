3df5f3f7058306c786b4ccc82e8e53a4e4ed1f85
Éric Bischoff <bischoff@kde.org>
2001-09-22 10:45:39 +0000

    New "penguin" gameboard added The corresponding masks and layout are not correct yet
    svn path=/trunk/kdegames/ktuberling/; revision=115059

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 static const char *description = I18N_NOOP("Potato game for kids");
 static const char *text = I18N_NOOP("A program by Eric Bischoff <e.bischoff@noos.fr>\nand John Calhoun.\n\nThis program is dedicated to my daughter Sunniva.");
 
-static const char *version = "v0.2.2";
+static const char *version = "v0.2.3";
 
 
 int main(int argc, char *argv[])
