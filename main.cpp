fe7d163cbe53aa2bc4463097b6d7507de3120713
Éric Bischoff <bischoff@kde.org>
2002-01-14 09:16:03 +0000

    CVS_SILENT New copyright, credits, and about message
    svn path=/trunk/kdegames/ktuberling/; revision=131576

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@
 
 
 static const char *description = I18N_NOOP("Potato game for kids");
-static const char *text = I18N_NOOP("A program by Eric Bischoff <e.bischoff@noos.fr>\nand John Calhoun.\n\nThis program is dedicated to my daughter Sunniva.");
+static const char *text = I18N_NOOP("A program by Eric Bischoff <e.bischoff@noos.fr>\nand John Calhoun.\n\nThis program is dedicated to my daughter Sunniva.\n\nYou need OGG Vorbis rc3 to be able to hear the sounds.");
 
-static const char *version = "0.2.7";
+static const char *version = "0.2.8";
 
 // Main function
 int main(int argc, char *argv[])
@@ -24,11 +24,12 @@ int main(int argc, char *argv[])
 
   KAboutData aboutData( "ktuberling", I18N_NOOP("KTuberling"), 
     version, description, KAboutData::License_GPL, 
-    "(c) 1999-2001, The KTuberling Developers", text);
+    "(c) 1999-2002, The KTuberling Developers", text);
   aboutData.addAuthor("Eric Bischoff", I18N_NOOP("Developer"), "e.bischoff@noos.fr");
   aboutData.addAuthor("John Calhoun", I18N_NOOP("Original concept and artwork"));
   aboutData.addCredit("Agnieszka Czajkowska", I18N_NOOP("New artwork"), "agnieszka@imagegalaxy.de");
   aboutData.addCredit("Bas Willems", I18N_NOOP("New artwork"), "next@euronet.nl");
+  aboutData.addCredit("Roger Larsson", I18N_NOOP("Sounds tuning"), "roger.larsson@norran.net");
   KCmdLineArgs::init(argc, argv, &aboutData);
 
   KApplication app;
