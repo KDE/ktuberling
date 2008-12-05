a79bc7da417d23a7397737b8e5a48240dd378e82
Albert Astals Cid <tsdgeos@terra.es>
2008-12-05 21:55:17 +0000

    increase version
    svn path=/trunk/KDE/kdegames/ktuberling/; revision=893076

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
 static const KLocalizedString description = ki18n("Potato game for kids");
 static const KLocalizedString text = ki18n("A program original by <email address='%1'>Éric Bischoff</email>\nand John Calhoun.\n\nThis program is dedicated to my daughter Sunniva.").subs("ebischoff@nerim.net");
 
-static const char version[] = "0.5.9";
+static const char version[] = "0.6.0";
 
 // Main function
 int main(int argc, char *argv[])
