ca96b6a063c8d1becbed611f0de3ad0c48e01573
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-09-19 15:51:11 +0200

    Drop override with final
    GIT_SILENT

--- toplevel.h
+++ toplevel.h
@@ -37,7 +37,7 @@ public:
 
   bool isSoundEnabled() const override;
 
-  void changeGameboard(const QString &gameboard) override final;
+  void changeGameboard(const QString &gameboard) final;
 
 protected:
   void readOptions(QString &board, QString &language);
