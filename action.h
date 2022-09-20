bed15483c203f294a102e0456c86d73cd13fdeaf
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-09-20 21:01:07 +0200

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- action.h
+++ action.h
@@ -1,12 +1,9 @@
-/***************************************************************************
- *   Copyright (C) 1999-2006 by Éric Bischoff <ebischoff@nerim.net>        *
- *   Copyright (C) 2007 by Albert Astals Cid <aacid@kde.org>               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- ***************************************************************************/
+/*
+    SPDX-FileCopyrightText: 1999-2006 Éric Bischoff <ebischoff@nerim.net>
+    SPDX-FileCopyrightText: 2007 Albert Astals Cid <aacid@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 /* Action stored in the undo buffer */
 
